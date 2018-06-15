7e11796864cc6b8318f99532bce071d85c47e311
Janiveer <janiveer@users.noreply.github.com>
2018-06-16 00:24:39 +0100

    Removed all status bar code

--- togglebars.py
+++ togglebars.py
@@ -7,9 +7,9 @@ import gtk
 class ToggleBars(geany.Plugin):
 
     __plugin_name__ = 'Toggle Bars'
-    __plugin_version__ = '0.4.0'
+    __plugin_version__ = '0.4.0bis'
     __plugin_description__ = \
-        'Show/hide the Geany menu and status bars with a keystroke'
+        'Show/hide the Geany menu bar with a keystroke'
     __plugin_author__ = 'Vasiliy Faronov <vfaronov@gmail.com>'
 
     def __init__(self):
@@ -22,18 +22,12 @@ class ToggleBars(geany.Plugin):
         # the main window's children to find the right kind of widget.
         # See also http://lists.geany.org/pipermail/devel/2016-July/010038.html
         self.menubar = find_widget(geany.main_widgets.window, is_main_menubar)
-        self.statusbar = find_widget(geany.main_widgets.window,
-                                     is_main_statusbar)
 
         self.currently_visible = None
         self.toggle(False)       # Hide initially
 
-        self.progressbar = geany.main_widgets.progressbar
-        self.handler1 = self.progressbar.connect('show', self.on_progress_show)
-        self.handler2 = self.progressbar.connect('hide', self.on_progress_hide)
-
         self.key_group = self.set_key_group('togglebars', 1)
-        self.key_group.add_key_item('toggle', 'Toggle menu and status bars',
+        self.key_group.add_key_item('toggle', 'Toggle menu bar',
                                     self.on_keybinding)
 
     def confirm(self):
@@ -57,7 +51,7 @@ class ToggleBars(geany.Plugin):
             return
         if show is None:
             show = not self.currently_visible
-        for widget in [self.menubar, self.statusbar]:
+        for widget in [self.menubar]:
             if widget is not None:
                 if show:
                     widget.show()
@@ -65,20 +59,8 @@ class ToggleBars(geany.Plugin):
                     widget.hide()
         self.currently_visible = show
 
-    def on_progress_show(self, *_args):
-        if self.confirmed:
-            if not self.currently_visible and self.statusbar is not None:
-                self.statusbar.show()
-
-    def on_progress_hide(self, *_args):
-        if self.confirmed:
-            if not self.currently_visible and self.statusbar is not None:
-                self.statusbar.hide()
-
     def cleanup(self):
         self.toggle(True)       # Show everything
-        self.progressbar.disconnect(self.handler1)
-        self.progressbar.disconnect(self.handler2)
 
 
 def create_empty_file(path):
@@ -104,8 +86,3 @@ def is_main_menubar(widget):
     return (isinstance(widget, gtk.MenuBar) and
             any(menu_item.get_submenu() is geany.main_widgets.tools_menu
                 for menu_item in widget.get_children()))
-
-
-def is_main_statusbar(widget):
-    return (isinstance(widget, gtk.Statusbar) and
-            find_widget(widget, lambda w: w is geany.main_widgets.progressbar))
